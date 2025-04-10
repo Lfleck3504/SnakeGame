ad74575eec302014f606cc729d251aebc9f47896
Lfleck3504 <lfleck3504@gmail.com>
2025-04-10 18:11:09 -0400

    Update Snake.ts

--- src/Snake.ts
+++ src/Snake.ts
@@ -2,35 +2,77 @@
 
 // place your code on line 5 above the export statement below
 
-class Snake {
-    private currentPosition: number;
-    private currentDirection: number;
-  
+
+  type Direction = "up" | "down" | "left" | "right";
+
+  class Snake {
+    
+    private currentPosition: Point;
+    private currentDirection: Direction;
+    
+    
     constructor() {
-      this.currentDirection = 1;
-      this.currentPosition = 0;
+      this.currentPosition = new Point(0, 0);
+      this.currentDirection = "right";
     }
-    public move(distance:number)
-    {
-      if (this.currentDirection === 1) {
-     
-        this.currentPosition += distance
-      } else 
-      {
-      this.currentPosition -= distance
+  
 
+  
+    get Direction(): Direction {
+      return this.currentDirection;
+    }
+  
+    public turnLeft(): void {
+      if (this.currentDirection === "up") {
+        this.currentDirection = "left";
+      } else if (this.currentDirection === "left") {
+        this.currentDirection = "down";
+      } else if (this.currentDirection === "down") {
+        this.currentDirection = "right";
+      } else if (this.currentDirection === "right") {
+        this.currentDirection = "up";
+      }
+    }
+  
+    public turnRight(): void {
+      if (this.currentDirection === "up") {
+        this.currentDirection = "right";
+      } else if (this.currentDirection === "right") {
+        this.currentDirection = "down";
+      } else if (this.currentDirection === "down") {
+        this.currentDirection = "left";
+      } else if (this.currentDirection === "left") {
+        this.currentDirection = "up";
+      }
+    }
+  
 
-       }
-
-
-
+    
+  
+    public move(): void {
+      const x = this.currentPosition.x;
+      const y = this.currentPosition.y;
+  
+      if (this.currentDirection === "up") {
+        this.currentPosition = new Point(x, y - 1);
+      } else if (this.currentDirection === "down") {
+        this.currentPosition = new Point(x, y + 1);
+      } else if (this.currentDirection === "left") {
+        this.currentPosition = new Point(x - 1, y);
+      } else if (this.currentDirection === "right") {
+        this.currentPosition = new Point(x + 1, y);
+      }
     }
 
-
-    /**
+      /**
      * turn
      */
-    public turn() {
+       /**
+     * @deprecated Use {turnLeft() or turnRight()} instead.
+     */ 
+    
+      /**
+      public turn() {
       if (this.currentDirection == 1) {
      
         this.currentDirection = -1
@@ -39,9 +81,13 @@ class Snake {
         this.currentDirection = 1
       }
   }
+  */
+
+  
   get position(){
     return this.currentPosition
   }
 }
+
   export default Snake;
   
